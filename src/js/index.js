155254a2fa125a16e1c221982f862b9a821dc52d
Emmanuel <154647800+egarboggini@users.noreply.github.com>
2023-12-27 04:20:04 -0300

    final?
    versão final do projeto seguindo as aulas. mas obviamente pode ser melhorado no futuro

--- src/js/index.js
+++ src/js/index.js
@@ -5,21 +5,21 @@ const botaoDeBusca = document.querySelector(".btn-busca");
 botaoDeBusca.addEventListener("click", async () => {
     const cidade = document.getElementById("input-busca").value;
 
-    if(!cidade) return;
+    if (!cidade) return;
 
     const dados = await buscarDadosDaCidade(cidade);
 
-    if(dados) preencherDadosNaTela(dados, cidade);
+    if (dados) preencherDadosNaTela(dados, cidade);
 });
 
 async function buscarDadosDaCidade(cidade) {
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
-    
+
     const resposta = await fetch(apiUrl);
 
-    if(resposta.status !== 200) return;
+    if (resposta.status !== 200) return;
 
-    const dados = resposta.json();
+    const dados = await resposta.json();
 
     return dados;
 }
@@ -28,19 +28,19 @@ function preencherDadosNaTela(dados, cidade) {
     const temperatura = dados.current.temp_c;
     const condicao = dados.current.condition.text;
     const umidade = dados.current.humidity;
-    const velocidadeDoVento = dados.current.condition.wind_kph;
+    const velocidadeDoVento = dados.current.wind_kph;
     const iconeCondicao = dados.current.condition.icon;
-    
+
     document.getElementById("cidade").textContent = cidade;
 
     document.getElementById("temperatura").textContent = `${temperatura} °C`;
 
     document.getElementById("condicao").textContent = condicao;
 
-    document.getElementById("umidade".textContent = `${umidade}%`);
+    document.getElementById("umidade").textContent = `${umidade}%`;
 
-    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento}km/h`;
+    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`;
 
-    document.getElementById("icone-condicao").setAttribute("src, iconeCondicao");
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
 }
 
